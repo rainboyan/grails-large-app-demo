889697fa103978e6fe46ca21127d6a75f4dae994
Michael Yan <rain@rainboyan.com>
2022-02-22 23:43:22 +0800

    Create large app

---
+++ grails-app/init/org/grails/demo/Application.groovy
@@ -0,0 +1,19 @@
+package org.grails.demo
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+import groovy.transform.CompileStatic
+// import org.springframework.context.annotation.ComponentScan
+
+@CompileStatic
+// @ComponentScan
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        def now = System.currentTimeMillis()
+
+        GrailsApp.run(Application, args)
+
+        println "${System.currentTimeMillis()-now}ms"
+    }
+}
